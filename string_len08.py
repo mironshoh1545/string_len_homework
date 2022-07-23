0e06fc299812e6748ec2ffe589af591a1f21d3ce
Mironshoh <mironshoh1545@gmail.com>
2022-07-23 08:28:08 +0500

    Barchasi

--- string_len08.py
+++ string_len08.py
@@ -1,6 +1,6 @@
 def main(s):
     """
-    Given variable type string s. Return the character in the muddle.
+    Given variable type string s. Return the character in the middle.
     If the length is even, return two characters in the middle.
 
     Args:
@@ -10,5 +10,5 @@ def main(s):
     """
     k = len(s)
     if k % 2 == 1:
-        return s[k // 2 + 1]
-    return s[k // 2] + s[k // 2 + 1]
+        return s[k // 2]
+    return s[k // 2 - 1] + s[k // 2]
--- string_len09.py
+++ string_len09.py
@@ -9,4 +9,4 @@ def main(num1, num2):
     Returns:
         str: answer
     """
-    return str(num1 + num2)
+    return str(int(num1) + int(num2))
